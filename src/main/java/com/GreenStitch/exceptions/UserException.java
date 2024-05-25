32c82f4f9991bd52f152c2b99a3dc41cf0fbc6b4
Shyam Shukla <shuklashyam279@gmail.com>
2024-05-25 12:53:12 +0530

    Assignment completed

---
+++ src/main/java/com/GreenStitch/exceptions/UserException.java
@@ -0,0 +1,12 @@
+package com.GreenStitch.exceptions;
+
+public class UserException extends Exception {
+
+	public UserException() {
+	}
+
+	public UserException(String message) {
+		super(message);
+	}
+
+}
---
+++ src/main/java/com/GreenStitch/jwt/SecurityConstants.java
@@ -0,0 +1,7 @@
+package com.GreenStitch.jwt;
+
+public interface SecurityConstants {
+
+	public static final String JWT_KEY ="demouser$77$demoUser~shkid99~";
+	public static final String JWT_HEADER ="Authorization";
+}
